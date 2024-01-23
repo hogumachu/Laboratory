75b6d6997c198ec551732d8416e49e73807f1838
hogumachu <castle_jun@daum.net>
2024-01-23 13:05:38 +0900

    Cache Sample App 추가

---
+++ CacheSample/CacheSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CacheSample
+//
+//  Created by 홍성준 on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CacheSample/CacheSampleTests/CacheSampleTests.swift
@@ -0,0 +1,20 @@
+//
+//  CacheSampleTests.swift
+//  CacheSampleTests
+//
+//  Created by 홍성준 on 1/23/24.
+//
+
+import XCTest
+
+final class CacheSampleTests: XCTestCase {
+    
+    func test_반드시_성공_1()  {
+        XCTAssertEqual(true, true)
+    }
+    
+    func test_반드시_성공_2()  {
+        XCTAssertEqual(false, false)
+    }
+    
+}
